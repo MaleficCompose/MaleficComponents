9e98cd930ceebd9ab1b8d21c60ad373c25638692
Om Gupta <ogupta4242@gmail.com>
2025-08-30 16:43:59 -0700

    chore(gradle): remove unnecessary deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,10 +46,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(compose.ui)
-                implementation(compose.foundation)
                 implementation(compose.material)
-                implementation(compose.animation)
                 implementation(libs.malefic.theming)
             }
         }
